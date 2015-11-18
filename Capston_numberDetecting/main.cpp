eb5670c6b7f620e7cea5cff514cc99e80058009e
ony Kwon T <Tony@tonyui-imac.local>
2015-11-18 15:06:15 +0900

    Initial Commit

---
+++ Capston_numberDetecting/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Capston_numberDetecting
+//
+//  Created by Tony Kwon on 2015. 11. 18..
+//  Copyright © 2015년 Tony Kwon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
